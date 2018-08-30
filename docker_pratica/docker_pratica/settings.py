c53b20778e901d624382fcfb91ed7e197a5c9cc8
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2018-08-29 22:45:35 -0300

    Created others examples

--- docker_pratica/docker_pratica/settings.py
+++ docker_pratica/docker_pratica/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'docker_pratica.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'register',
-        'USER': 'master',
-        'PASSWORD': 'master',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'register',
+    #     'USER': 'master',
+    #     'PASSWORD': 'master',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
